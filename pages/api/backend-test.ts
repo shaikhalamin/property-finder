72f01c0daa452e18995361c2b20ee786c94c910f
Shaikh Al Amin <alamin.cse15@gmail.com>
2023-08-04 22:51:37 +0600

    vercel serverless cold start issue fixing

--- pages/api/backend-test.ts
+++ pages/api/backend-test.ts
@@ -1,9 +1,9 @@
 import { API_BASE } from "@/data/utils/api.urls";
 import { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
 
 const backendTest = async (req:NextApiRequest, res:NextApiResponse) => {
-  const response = await fetch(API_BASE as string);
-  const data = await response.json();
-  res.status(200).json({ name: data });
+  const response = await axios(API_BASE as string);
+  res.status(200).json({ name: response.data });
 }
 export default backendTest;
