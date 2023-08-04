4a881246576fea8a67b276e909fe3c24f046c7bf
Shaikh Al Amin <alamin.cse15@gmail.com>
2023-08-04 22:10:18 +0600

    Update backend-test.ts

--- pages/api/backend-test.ts
+++ pages/api/backend-test.ts
@@ -1,6 +1,6 @@
 import { API_BASE } from "@/data/utils/api.urls";
 import { NextApiRequest, NextApiResponse } from "next";
 export default function handler(req:NextApiRequest, res:NextApiResponse) {
-  const res = await fetch(API_BASE);
-  res.status(200).json({ name: res });
+  const response = await fetch(API_BASE);
+  res.status(200).json({ name: response });
 }
