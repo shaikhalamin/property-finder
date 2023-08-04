0b91fa6fae15879e22ec4022a1890e30f8ad0ce3
Shaikh Al Amin <alamin.cse15@gmail.com>
2023-08-04 22:59:44 +0600

    track cronjob call with logging

--- pages/api/backend-test.ts
+++ pages/api/backend-test.ts
@@ -2,8 +2,14 @@ import { API_BASE } from "@/data/utils/api.urls";
 import { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 
-const backendTest = async (req:NextApiRequest, res:NextApiResponse) => {
+const backendTest = async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log(
+    "cronjob:call --> ",
+    `Cron job called at ${new Date().toLocaleString("en-US", {
+      timeZone: "Asia/Dhaka",
+    })}`
+  );
   const response = await axios(API_BASE as string);
   res.status(200).json({ name: response.data });
-}
+};
 export default backendTest;
