6cb20bab0d471e7566c3483e4054ba8aaf56a9d1
Shaikh Al Amin <alamin.cse15@gmail.com>
2023-08-04 22:14:29 +0600

    Update backend-test.ts

--- pages/api/backend-test.ts
+++ pages/api/backend-test.ts
@@ -2,7 +2,7 @@ import { API_BASE } from "@/data/utils/api.urls";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const backendTest = async (req:NextApiRequest, res:NextApiResponse) => {
-  const response = await fetch(API_BASE);
+  const response = await fetch(API_BASE as string);
   res.status(200).json({ name: response });
 }
 export default backendTest;
